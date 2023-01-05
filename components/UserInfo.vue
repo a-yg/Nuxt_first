8d7faf8bb67b1d79fdd55dfe08268dd0b2dd4100
a-yg <a.shoga10@gmail.com>
2023-01-05 19:54:39 +0900

    provide.inject

---
+++ components/UserInfo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div>
+      入力したユーザー名：{{userName}}
+    </div>
+    <div>
+      入力した年齢：{{userAge}}
+    </div>
+    <div>
+      入力した住所：{{userAddress}}
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+import { inject } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const userName = inject('inputUserName')
+    const userAge = inject('inputUserAge')
+    const userAddress = inject('inputUserAddress')
+
+    return {
+      userName,
+      userAge,
+      userAddress
+    }
+  },
+})
+</script>
+
+<style scoped>
+.container  {
+  border: solid;
+}
+</style>
---
+++ pages/UserInput.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <td>ユーザー名</td>
+        <td><input type="text" v-model="userName"></td>
+      </tr>
+      <tr>
+        <td>年齢</td>
+        <td><input type="text" v-model="userAge"></td>
+      </tr>
+      <tr>
+        <td>住所</td>
+        <td><input type="text" v-model="userAddress"></td>
+      </tr>
+    </table>
+    <UserInfo />
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref, provide } from '@nuxtjs/composition-api'
+import UserInfo from '../components/UserInfo.vue'
+
+export default defineComponent({
+  components: {
+    UserInfo
+  },
+  setup() {
+    const userName = ref()
+    const userAge = ref()
+    const userAddress = ref()
+
+    provide('inputUserName', userName)
+    provide('inputUserAge', userAge)
+    provide('inputUserAddress', userAddress)
+
+    return {
+      userName,
+      userAge,
+      userAddress
+    }
+
+  },
+})
+</script>
